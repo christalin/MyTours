e3b67a9175f323e095b4554321f81b238107c3f3
christalin <christy0309@gmail.com>
2016-07-30 13:53:55 -0500

    Changed project to MVC 6 Framework

---
+++ src/MyTours/Controller/Web/AppController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using MyTours.Services;
+using MyTours.ViewModels;
+
+namespace MyTours.Controller.Web
+{
+    public class AppController : Microsoft.AspNetCore.Mvc.Controller
+    {
+        private IMailService _mailService;
+        private IConfigurationRoot _config;
+
+        public AppController(IMailService mailService, IConfigurationRoot config)
+        {
+            _mailService = mailService;
+            _config = config;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Contact(ContactViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                _mailService.SendMail(_config["MailSettings:ToAddress"], model.Email, "From TheWorld", model.Message);
+
+                ModelState.Clear();
+
+                ViewBag.UserMessage = "Message Sent";
+            }
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+    }
+}
---
+++ src/MyTours/Services/DebugMailService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyTours.Services
+{
+    public class DebugMailService : IMailService
+    {
+        void IMailService.SendMail(string to, string from, string subject, string body)
+        {
+           Debug.WriteLine($"Sending Mail: To: {to} From: {from} Subject: {subject}");
+        }
+    }
+}
